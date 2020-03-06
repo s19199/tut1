7b2be74ab942bd2bd5be7982cbe4514c36a421d2
Assem Kozhekova <s19199@pjwstk.edu.pl>
2020-03-06 13:46:58 +0100

    started task3

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+     class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://www.pja.edu.pl";
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+            var regex = new Regex(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z");
+            var content = response.Content;
+            var matches = regex.Matches(content.ToString());
+
+            foreach (var match in matches) 
+            {
+            Console.WriteLine(match.ToString());
+            }
+
+
+           // Console.WriteLine("Hello World!");
+        }
+    }
+}
